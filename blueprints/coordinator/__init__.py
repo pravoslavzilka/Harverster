746d60d4bb3bc0e2affb895e1f41580af0718e25
Pravoslav <pravoslav.zilka@gmail.com>
2022-11-17 15:15:56 +0100

    Login system base code

--- blueprints/coordinator/__init__.py
+++ blueprints/coordinator/__init__.py
@@ -1,9 +1,59 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash, session
+from flask_login import login_required, current_user, login_user, logout_user
+from models import User, Coordinator
 
 
 coordinator_bp = Blueprint("coordinator_bp", __name__, template_folder="templates")
 
 
 @coordinator_bp.route("/")
+@login_required
 def main_page():
     return "hello coordinator"
+
+
+@coordinator_bp.route("/sign-in", methods=['GET'])
+def sign_in_view():
+    if current_user.is_authenticated:
+        return redirect(url_for("welcome_page"))
+
+    return render_template("coordinator/sign_in_page.html")
+
+
+@coordinator_bp.route("/sign-in", methods=['POST'])
+def sign_in_fun():
+    email = request.form["email"]
+    password = request.form["password"]
+
+    coordinator = Coordinator.query.filter(Coordinator.email == email).first()
+
+    if coordinator and coordinator.check_password(password):
+
+        login_user(coordinator)
+        flash("Welcome back", "success")
+        return redirect(url_for("coordinator_bp.main_page"))
+
+    admin = User.query.filter(User.email == email).first()
+    if admin and admin.check_password(password):
+
+        login_user(admin)
+
+        session["permit"] = 1
+
+        flash("Welcome back", "success")
+        return redirect(url_for("admin_bp.main_page"))
+
+    flash("Invalid credentials", "danger")
+    return render_template("coordinator/sign_in_page.html")
+
+
+@coordinator_bp.route("/logoff")
+@login_required
+def sign_off():
+    if "permit" in session:
+        session.pop("permit")
+
+    logout_user()
+    flash("You've been signed off", "success")
+    return redirect(url_for("welcome_page"))
+
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from blueprints.admin.__init__ import admin_bp
 from blueprints.super_admin.__init__ import super_admin_bp
 from blueprints.coordinator.__init__ import coordinator_bp
 from blueprints.warehouse.__init__ import warehouse_bp
+from flask_login import LoginManager
+from database import db_session
+from models import User, Coordinator
+import os
 
 app = Flask(__name__)
 
@@ -12,9 +16,39 @@ app.register_blueprint(coordinator_bp, url_prefix="/coordinator")
 app.register_blueprint(warehouse_bp, url_prefix="/warehouse")
 
 
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(16)
+app.config["SESSION_COOKIE_HTTPONLY"] = True
+app.config["REMEMBER_COOKIE_HTTPONLY"] = True
+app.config["SESSION_COOKIE_SECURE"] = True
+app.config["REMEMBER_COOKIE_SECURE"] = True
+app.config["SESSION_COOKIE_SAMESITE"] = 'Lax'
+
+UsersStatus = []
+
+app.jinja_env.autoescape = True | False
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = ""
+login_manager.login_message = "Sign in"
+login_manager.login_message_category = "info"
+login_manager.session_protection = "strong"
+
+
 @app.route("/")
 def welcome_page():
     return render_template("index.html")
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Coordinator.query.filter(Coordinator.id == user_id).first() or User.query.filter(User.id == user_id).first()
 
 
 if __name__ == "__main__":

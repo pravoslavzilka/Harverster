ef777e86e24d676a00bfe640f2e6a54fe5732175
Pravoslav <pravoslav.zilka@gmail.com>
2022-11-20 15:50:48 +0100

    Admin place order

--- blueprints/admin/__init__.py
+++ blueprints/admin/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash, session
+from database import db_session
 from functools import wraps
 from models import *
+import datetime
 
 
 admin_bp = Blueprint("admin_bp", __name__, template_folder="templates")
@@ -32,7 +34,21 @@ def new_order_view():
 @admin_bp.route("/new-order", methods=['POST'])
 @check_admin
 def new_order_fun():
+    items = request.form["items_total"].split(",")
+    maxes = request.form["maxes_total"].split(",")
 
-    print(request.form["items_total"])
-    print(request.form["maxes_total"])
-    return redirect(url_for('admin_bp.new_order_view'))
+    order_content = {items[i]: maxes[i] for i in range(len(items))}
+    print(order_content)
+    hubs = Hub.query.all()
+    for hub in hubs:
+        new_order = Order()
+        new_order.hub = hub
+        new_order.status = 1
+        new_order.content = order_content
+        new_order.time = datetime.datetime.now()
+        db_session.add(new_order)
+
+    db_session.commit()
+
+    flash("New order was placed", "success")
+    return redirect(url_for('admin_bp.main_page'))

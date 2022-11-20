a8f736af52b3ef3c97547b13206d301b26531d67
Pravoslav <pravoslav.zilka@gmail.com>
2022-11-20 12:51:19 +0100

    New design for order

--- blueprints/admin/__init__.py
+++ blueprints/admin/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash, session
 from functools import wraps
+from models import *
 
 
 admin_bp = Blueprint("admin_bp", __name__, template_folder="templates")
@@ -19,3 +20,10 @@ def check_admin(func):
 @check_admin
 def main_page():
     return render_template("admin/main_dashboard.html")
+
+
+@admin_bp.route("/new-order")
+@check_admin
+def new_order_view():
+    items = Item.query.all()
+    return render_template("admin/new_order.html", items=items)
